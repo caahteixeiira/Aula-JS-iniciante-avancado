20d48f8a4e48e90ea28578c03af15d9dd78175b3
Caroline Teixeira Bacerlos Costa <caroline.costa@cieemg.bhz>
2024-08-14 17:11:22 -0300

    atualizando

--- Aulas - SEÇÃO4/aula58 - 77/index.js
+++ Aulas - SEÇÃO4/aula58 - 77/index.js
@@ -7,18 +7,10 @@ const cliqueDel = document.querySelector('.btn-del');
 const cliqueEqual = document.querySelector('.btn-eq');
 const displayCal = document.querySelector('.display');
 
-document.addEventListener('click', (e) => {
-    const el = e.target;
 
-    if(el.classList.contains('btn-num')){
-        btnNum(el.innerText);
-        console.log('teste')
-    }
-     
-});
 
-function btnNum(valor){
-    displayCal.valor += valor;
+function displayInicial(valor){
+    cliqueNum.value += valor;
+    console.log(cliqueNum.value);
 };
-
 
